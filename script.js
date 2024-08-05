7de5ccde0386851929e9d896d1983cd5e867dd36
RITESH0091 <95367480+RITESH0091@users.noreply.github.com>
2024-08-05 21:53:28 +0530

    Add existing file

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fix-image")
+elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none"
+})
+
+// var elem1 = document.querySelector("#elem1")
+// elem1.addEventListener("mouseenter", function () {
+//     var imgae = elem1.getAttribute("data-imgae");
+//     fixed.style.backgroundImage = `url(${imgae})`
+// })
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter", function(){
+        var image = e.getAttribute("data-imgae")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
---
+++ test.js
@@ -0,0 +1,3 @@
+var a = document.querySelector("img")
+var b = a.getAttribute("data-name")
+console.log(b)
