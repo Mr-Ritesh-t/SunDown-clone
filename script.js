e6f3bc398e86d7afb440a744cfb008711fca813b
RITESH0091 <95367480+RITESH0091@users.noreply.github.com>
2024-08-07 00:24:43 +0530

    Ritesh

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-var elemC = document.querySelector("#elem-container")
+function page4Animation(){
+
+    var elemC = document.querySelector("#elem-container")
 var fixed = document.querySelector("#fix-image")
 elemC.addEventListener("mouseenter", function () {
     fixed.style.display = "block"
@@ -12,16 +14,27 @@ elemC.addEventListener("mouseleave", function () {
     fixed.style.display = "none"
 })
 
-// var elem1 = document.querySelector("#elem1")
-// elem1.addEventListener("mouseenter", function () {
-//     var imgae = elem1.getAttribute("data-imgae");
-//     fixed.style.backgroundImage = `url(${imgae})`
-// })
-
-var elems = document.querySelectorAll(".elem")
+    var elems = document.querySelectorAll(".elem")
 elems.forEach(function(e){
     e.addEventListener("mouseenter", function(){
         var image = e.getAttribute("data-imgae")
         fixed.style.backgroundImage = `url(${image})`
     })
 })
+}
+
+page4Animation()
+
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 100,
+        freeMode: true,
+    //     pagination: {
+    //       el: ".swiper-pagination",
+    //       clickable: true,
+    //     },
+      });
+}
+
+swiperAnimation()
--- test.js
+++
@@ -1,3 +0,0 @@
-var a = document.querySelector("img")
-var b = a.getAttribute("data-name")
-console.log(b)
